524c0383c035612d971cc92165b8e67ac77131e2
DongNQ225284 <nguyendong.vn2004@gmail.com>
2025-08-07 11:15:53 +0700

    upload

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO  # type: ignore
 
 if __name__ == "__main__":
-    model = YOLO("runs/detect/train4/weights/last.pt")
-    model.train(data="Fire_indoor_data/data.yaml", batch = 20, epochs=100, imgsz=416)
+    model = YOLO("runs/detect/train5/weights/last.pt")
+    model.train(data="Fire_indoor_data/data.yaml", batch = 25, epochs=100, imgsz=416)
     
